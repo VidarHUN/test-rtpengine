accc4fff26ea1cc74d26840fc291118d53526443
Richárd Váradi <ricsi19981007@gmail.com>
2020-12-14 17:58:09 +0100

    Be able to generate traffic from python script

--- clients/python/app.py
+++ clients/python/app.py
@@ -3,6 +3,7 @@ import sdp_transform # pip install sdp-transform
 import argparse
 import json
 import time
+import subprocess
 import socket
 import sys
 import random
@@ -23,6 +24,10 @@ parser.add_argument('--bind_offer', '-bo', nargs=2, default=['127.0.0.1', '2000'
 parser.add_argument('--bind_answer', '-ba', nargs=2, default=['127.0.0.1', '2004'], dest='bind_answer',
                     help='address port for answer source')
 parser.add_argument('--file', '-f', type=str, dest='file', help="A simple file to list or query")
+parser.add_argument('--ffmpeg', '-ff', type=int, dest='ffmpeg', 
+                    help="If specified, it will start a certain number of ffmpeg processes")
+parser.add_argument('--audio_file', '-af', type=str, dest='audio_file', help="Path of the audio to ffmpeg")
+parser.add_argument('--tcpdump', '-t', type=str, dest='tcpdump', help='tcpdump interface')
 
 # Proxy part
 parser.add_argument('--server', '-s', type=int, dest='server', choices=[0,1], 
@@ -42,6 +47,7 @@ bc = bencodepy.Bencode(
 def gen_cookie(length):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
+# Send traffic to rtpengine
 def send(file, bind_address, bind_port):
     # Open UDP4 socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -66,6 +72,22 @@ def send(file, bind_address, bind_port):
     
     return result
 
+# Run a certain number of ffmpeg command 
+def ffmpeg(cnt, offer_rtp_address, answer_rtp_address):
+    procs = []
+    for _ in range(cnt):
+        procs.append(subprocess.Popen(["ffmpeg", "-re", "-i", args.audio_file, "-ar", "8000", "-ac", "1", "-acodec", "pcm_mulaw", "-f", "rtp", offer_rtp_address]))
+        procs.append(subprocess.Popen(["ffmpeg", "-re", "-i", args.audio_file, "-ar", "8000", "-ac", "1", "-acodec", "pcm_mulaw", "-f", "rtp", answer_rtp_address]))
+    for proc in procs:
+        proc.communicate()
+
+# Run tcpdump in the background on a given interface
+def tcpdump():
+    return subprocess.Popen(["sudo", "tcpdump", "-i", args.tcpdump, "udp", "-vvn", "-w", "traffic.pcap"])
+
+if args.tcpdump:
+    tcpdump_proc = tcpdump()
+
 if not args.server:
     if args.file:
         with open(args.file) as f:
@@ -78,15 +100,19 @@ if not args.server:
             offer = json.load(o)
         response = send(offer, args.bind_offer[0], int(args.bind_offer[1]))
         parsed_sdp_dict = sdp_transform.parse(response.get('sdp'))
-        print("RTP port from offer: %d" % parsed_sdp_dict.get('media')[0].get('port'))
-        print("RTCP port from offer: %d" % parsed_sdp_dict.get('media')[0].get('rtcp').get('port'))
+        offer_rtp_port = parsed_sdp_dict.get('media')[0].get('port')
+        offer_rtcp_port = parsed_sdp_dict.get('media')[0].get('rtcp').get('port')
+        print("RTP port from offer: %d" % offer_rtp_port)
+        print("RTCP port from offer: %d" % offer_rtcp_port)
     if args.answer:
         with open(args.answer) as a:
             answer = json.load(a)
         response = send(answer, args.bind_answer[0], int(args.bind_answer[1]))
         parsed_sdp_dict = sdp_transform.parse(response.get('sdp'))
-        print("RTP port from answer: %d" % parsed_sdp_dict.get('media')[0].get('port'))
-        print("RTCP port from answer: %d" % parsed_sdp_dict.get('media')[0].get('rtcp').get('port'))
+        answer_rtp_port = parsed_sdp_dict.get('media')[0].get('port')
+        answer_rtcp_port = parsed_sdp_dict.get('media')[0].get('rtcp').get('port')
+        print("RTP port from answer: %d" % answer_rtp_port )
+        print("RTCP port from answer: %d" % answer_rtcp_port)
 else:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((args.server_address, args.server_port))
@@ -98,6 +124,15 @@ else:
         parsed_sdp_dict = sdp_transform.parse(response.get('sdp'))
         print("RTP port from rtpengine: %d" % parsed_sdp_dict.get('media')[0].get('port'))
         print("RTCP port from rtpengine: %d\n" % parsed_sdp_dict.get('media')[0].get('rtcp').get('port'))
+
+if args.offer and args.answer:
+    time.sleep(1)
+    offer_rtp_address = "rtp://127.0.0.1:" + str(offer_rtp_port) + "?localrtpport=" + str(args.bind_offer[1])
+    answer_rtp_address = "rtp://127.0.0.1:" + str(answer_rtp_port) + "?localrtpport=" + str(args.bind_answer[1])
+    ffmpeg(int(args.ffmpeg), offer_rtp_address, answer_rtp_address)
+
+if args.tcpdump:
+    tcpdump_proc.terminate()
 
 # TODO: Somehow make statistics about the traffic quality 
 # Maybe you van use pyshark or a tcpdump subprocess
